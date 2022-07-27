16a56ffdb95b65c5d2fd811599da35673fbd5dd3
kagi <k4gi@pm.me>
2022-07-27 12:40:12 +1000

    tile types for real

--- tile_types.py
+++ tile_types.py
@@ -7,7 +7,7 @@ import numpy as np
 #what the fuck are these comments supposed to mean
 graphic_dt = np.dtype( [
 	("ch", np.int32),
-	("fg", "3B"),
+	("fg", "3B"), #3B means 3 unsigned bytes
 	("bg", "3B"),
 ] )
 
@@ -28,5 +28,5 @@ def new_tile(
 		return np.array((walkable,transparent,dark), dtype=tile_dt)
 
 
-floor = new_tile(walkable=True, transparent=True, dark=(ord(" "), (255,255,255), (50,50,150)))
-wall = new_tile(walkable=False, transparent=False, dark=(ord(" "), (255,255,255), (0,0,100)))
+floor = new_tile(walkable=True, transparent=True, dark=(ord("."), (255,255,255), (50,50,150)))
+wall = new_tile(walkable=False, transparent=False, dark=(ord("#"), (255,255,255), (0,0,100)))
