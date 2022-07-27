bcf93e6089ebca9f7d2d10df38062b82e662f91d
k4gi <k4gi@pm.me>
2022-07-27 11:56:25 +1000

    engine ?

---
+++ engine.py
@@ -0,0 +1,8 @@
+from typing import Set, Iterable, Any
+
+from tcod.context import Context
+from tcod.console import Console
+
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+from entity import Entity
